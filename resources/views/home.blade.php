1cd159ffb90942099521bd6cd51de1f2b80e4462
Rael Corrales <raelcorrales@gmail.com>
2018-05-21 12:31:29 -0500

    Home Traducido Beta

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -40,15 +40,15 @@
           <div class="col-md-9 wow fadeInLeft animated">
             <div>
               <p>
-                El Dr. Sergio del Hoyo cirujano gastrointestinal (gastroenterólogo) con adiestramiento en laparoscopia (cirugía de mínima invasión), cirugía de obesidad y enfermedades metabólicas (cirugía para la obesidad). Cuenta con 15 años de experiencia en cirugía Gastrointestinal y enfermedades gastrointestinales y más de 7 años en cirugía para la obesidad (Manga gástrica y Bypass gástrico).
+                @lang('home.who-we-are')
               </p>
             </div>
             <div class="col-md-6">
               <div class="home_team_med">
                 <span class="icon-invoice"></span>
-                <h4>Equipo Médico Certificado</h4>
+                <h4>@lang('home.who-team')</h4>
                 <p>
-                  Somos un equipo conformado por especialistas en Gastroenterología, Cirugía Gastrointestinal, Anestesiología, Medicina interna, Terapia intensiva, Psicología y Nutrición. Disponibles siempre a ayudarle en todo momento que usted lo requiera. Bilingües en los idiomas español e inglés.
+                  @lang('home.who-team-desc')
                 </p>
                 <a href=""> Conozca Más </a>
               </div>
@@ -58,30 +58,20 @@
             <div class="col-md-6">
               <div class="home_care">
                 <span class="icon-care"></span>
-                <h4>Ética, responsabilidad, preparación y actualización constante</h4>
-                <p>
-                  Brindamos siempre atención médica de calidad y seguridad a todos nuestros pacientes. Somos un equipo médico CERTIFICADO en nuestra especialidad lo que traduce certeza y confianza en nuestra atención médica haciendo diagnósticos y tratamientos especializados.
-                </p>
-                <p>
-                  Dr. Sergio del Hoyo y su Equipo acuden año con año a diversos cursos y congresos nacionales e internacionales.
-                </p>
+                <h4>@lang('home.care')</h4>
+                @lang('home.care-desc')
               </div>
             </div>
 
           </div>
           <div class="col-md-3 wow fadeInRight animated">
             <div>
-              <h3 class="text-uppercase small-line mb20">Atención médica <span>las 24 hrs.</span></h3>
-              <p>
-                Disponibles los 7 días de la semana
-                Atención especialidaza en consulta programada Lunes a Sábado de 9 a 20 hrs.
-              </p>
+              <h3 class="text-uppercase small-line mb20">@lang('home.atencion')</h3>
+              @lang('home.atencion-desc')
             </div>
             <div>
-              <h3 class="text-uppercase small-line mb20">Urgencias</h3>
-              <p>
-                Las 24 horas del día con atención en los mejores hospitales de Puerto Vallarta y Nuevo Vallarta.
-              </p>
+              <h3 class="text-uppercase small-line mb20">@lang('home.urgencia')</h3>
+              @lang('home.urgencia-desc')
             </div>
 
           </div>
@@ -95,31 +85,29 @@
         <div class="row">
             <div class="imc-message col-md-4 col-sm-6 col-xs-12">
 
-              <h3 class="text-uppercase small-line mb20">CALCULADORA <span>IMC</span></h3>
-              <p>
-                Conozca su Índice de Masa Corporal, ingrese su estatura en cms y peso en Kg
-              </p>
+              <h3 class="text-uppercase small-line mb20">@lang('home.imc')</h3>
+              @lang('home.imc-desc')
             </div>
             <div class="imc-form col-md-4 col-sm-6 col-xs-12">
               <div class="col-md-12">
                 <div class="metrico col-md-4">
-                  <input type="radio" name="imc-type" /> Métrico
+                  <input type="radio" name="imc-type" /> @lang('home.metrico')
                 </div>
                 <div class="imperial col-md-4">
-                  <input type="radio" name="imc-type" /> Imperial
+                  <input type="radio" name="imc-type" /> @lang('home.imperial')
                 </div>
               </div>
 
               <div class="col-md-12">
                 <div class="altura col-md-6">
                   <label>
-                    Altura
+                    @lang('home.altura')
                     <input type="text" />
                   </label>
                 </div>
                 <div class="peso col-md-6">
                   <label>
-                    Peso
+                    @lang('home.peso')
                     <input type="text" />
                   </label>
                 </div>
@@ -137,10 +125,10 @@
 
             </div>
             <div class="imc-resultado col-md-4 col-sm-6 col-xs-12">
-              <div class="imc-de"> Tu IMC es de:</div>
-              <div class="imc-res"> 20.5 </div>
-              <div class="imc-range"> 18.5 a 20 </div>
-              <div class="imc-desc"> Desorden alimenticio </div>
+              <div class="imc-de"> @lang('home.tu-imc')</div>
+              <div class="imc-res"> @lang('home.tu-imc-res') </div>
+              <div class="imc-range"> @lang('home.tu-imc-range') </div>
+              <div class="imc-desc"> @lang('home.tu-imc-desc') </div>
             </div>
           </div>
       </div>
@@ -150,22 +138,22 @@
       <div class="container">
         <div class="section-content">
           <div class="row">
-            <h3 class="col-md-12">El Dr. Sergio del Hoyo es miembro de:</h3>
+            <h3 class="col-md-12">@lang('home.es-miembro')</h3>
             <div class="rasoc col-md-3">
               <img src="{{ asset('images/asoc/asociacion-mexicana-de-cirugia-general.png') }}" />
-              <span> Asociación Mexicana de Cirugía General </span>
+              <span> @lang('home.es-miembro-asoc-mex') </span>
             </div>
             <div class="rasoc col-md-3">
               <img src="{{ asset('images/asoc/asociacion-mexicana-de-cirugia-endoscopica-ac.png') }}" />
-              <span> Asociación Mexicana de Cirugía Endoscópica, A.C. </span>
+              <span> @lang('home.es-miembro-asoc-end') </span>
             </div>
             <div class="rasoc col-md-3">
               <img src="{{ asset('images/asoc/asociacion-mexicana-de-cirugia-general-2.png') }}" />
-              <span> Asociación Mexicana de Cirugía General</span>
+              <span> @lang('home.es-miembro-asoc-mex1') </span>
             </div>
             <div class="rasoc col-md-3">
               <img src="{{ asset('images/asoc/consejo-mexicano-de-cirugia-general-ac.png') }}" />
-              <span> Consejo Mexicano de Cirugía General, A.C. </span>
+              <span> @lang('home.es-miembro-asoc-cir') </span>
             </div>
           </div>
         </div>
